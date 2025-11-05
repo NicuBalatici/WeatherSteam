206361b9534b79db9f019d3bea4cea5ae1f4c5d0
NicuBalatici <nicu.marian28@yahoo.com>
2025-11-05 12:13:32 +0200

    weather api added

--- app/src/main/java/com/example/weathersteam/handlers/weatherApiHandler.kt
+++ app/src/main/java/com/example/weathersteam/handlers/weatherApiHandler.kt
@@ -1,5 +1,6 @@
 package com.example.weathersteam.handlers
 
+import com.example.weathersteam.data.WeatherData
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
@@ -7,18 +8,29 @@ import io.ktor.client.statement.bodyAsText
 import io.ktor.http.URLBuilder
 import org.json.JSONObject
 
-suspend fun weatherApiHandler(latitude: Double, longitude: Double) {
+suspend fun weatherApiHandler(latitude: Double, longitude: Double): WeatherData {
     val client = HttpClient(CIO)
 
-    val weatherUrl = URLBuilder("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&current=temperature_2m,rain,cloud_cover,snowfall,precipitation").build()
-    val response = client.get(weatherUrl)
-    val responseJson = JSONObject(response.bodyAsText()).getJSONObject("current")
+    try {
+        val weatherUrl = URLBuilder("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&current=temperature_2m,rain,cloud_cover,snowfall,precipitation").build()
+        val response = client.get(weatherUrl)
+        val responseJson = JSONObject(response.bodyAsText()).getJSONObject("current")
 
-    /**
-     * TODO: take relevant data from responseJson
-     * TODO: store data in a WeatherData object (create the WeatherData class)
-     * TODO: return the object
-     */
+        val temp = responseJson.getDouble("temperature_2m")
+        val rain = responseJson.getDouble("rain")
+        val cloudCover = responseJson.getInt("cloud_cover")
+        val snowfall = responseJson.getDouble("snowfall")
+        val precipitation = responseJson.getDouble("precipitation")
 
-    client.close()
+        return WeatherData(
+            temperature = temp,
+            rain = rain,
+            cloudCover = cloudCover,
+            snowfall = snowfall,
+            precipitation = precipitation
+        )
+
+    } finally {
+        client.close()
+    }
 }
